891b6998dc200bf97e051da7640a0014b12b9fe8
Popcorn <vincent.kastberg@hotmail.com>
2026-04-14 15:39:32 +0200

    Added integration test structure

---
+++ tests/ecs_tests.rs
@@ -0,0 +1,17 @@
+/* INTEGRATION TESTS OF ECS */
+
+#[cfg(test)]
+mod entity_tests {
+    #[test]
+    #[ignore = "Shitty test imo"]
+    fn testiiing() {
+        assert!(true);
+    }
+
+}
+
+#[cfg(test)]
+mod component_tests {}
+
+#[cfg(test)]
+mod system_tests {}
