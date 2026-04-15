f808fefd8eeace6bc7de014d91339d62aa27a28d
Popcorn <vincent.kastberg@hotmail.com>
2026-04-15 18:39:37 +0200

    Made project into a lib.rs
    This means it is a library, not a program. And therefore external folders and files can access our 'copper' library :)

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,5 @@
-mod ecs;
-use ecs::entity::*;
-use ecs::component::*;
-use ecs::system::*;
-use ecs::world::*;
-
-mod health_system;
-use health_system::*;
 
 
 fn main() {
-
-    let mut test_entity = Entity::new(1);
-
-    println!("{:?}", test_entity);
-
-    let mut test_component = Component::new("awd");
-
-    let prop1 = (String::from("Prop1"), PropertyData::Integer(50));
-
-    let prop2 = (
-        String::from("Prop2"),
-        PropertyData::Text(String::from("Hejsan")),
-    );
-
-    let prop3 = (String::from("Prop3"), PropertyData::Flag(true));
-
-    test_component.add_property(prop1);
-    test_component.add_property(prop2);
-    test_component.add_property(prop3);
     
-    println!("{:?}", test_component);
-    println!("{:?}", test_component.get_id());
-
-    test_entity.add_component(test_component);
-
-    let health_system = HealthSystem;
-    println!("{:#?}", health_system.get_component_types());
-
-    let mut world = World::new();
-
 }
--- tests/ecs_tests.rs
+++ tests/ecs_tests.rs
@@ -1,6 +1,8 @@
 /* INTEGRATION TESTS OF ECS */
 // Enter 'cargo test' to run tests
 
+use copper;
+
 #[cfg(test)]
 mod entity_tests {
     #[test]
@@ -12,7 +14,9 @@ mod entity_tests {
 }
 
 #[cfg(test)]
-mod component_tests {}
+mod component_tests {
+    
+}
 
 #[cfg(test)]
 mod system_tests {}
