ea2c1b738eef506601248c937a8c4bed24397559
Leo Erdös <leo.erdos.madegard@gmail.com>
2026-04-28 15:38:47 +0200

    new gamefile

---
+++ testgame2/src/main.rs
@@ -0,0 +1,19 @@
+use copper::engine::system::*;
+use copper::engine::world::*;
+use copper::engine::*;
+use copper::resource::Resources;
+use copper::*;
+use std::any::TypeId;
+use component_macro_derive::*;
+
+
+// #[derive(Component)]
+// struct TransformComponent {
+//     pos_x: i32,
+//     pos_y: i32,
+// }
+
+fn main() {
+    println!("Hello, world!");
+    let mut engine = Engine::new();
+}
