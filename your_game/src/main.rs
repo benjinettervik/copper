920b87a1e5eb46e8838a0ca9ea122c479de2c388
Popcorn <vincent.kastberg@hotmail.com>
2026-05-07 12:41:07 +0200

    Changed name of snake to your_game

---
+++ your_game/src/main.rs
@@ -0,0 +1,42 @@
+use copper::engine::system::*;
+use copper::engine::world::*;
+use copper::engine::*;
+use copper::renderer::render_sys::RenderSys;
+use copper::resource::Resources;
+use copper::*;
+use winit::event::KeyEvent;
+use winit::window;
+use std::any::TypeId;
+use component_macro_derive::*;
+
+use copper::input::input::*;
+use winit::keyboard::*;
+use winit::keyboard::KeyCode::*;
+
+
+
+
+
+
+struct SpawnSnakeSystem;
+impl System for SpawnSnakeSystem {
+    components_read!();
+    components_write!();
+    components_with!();
+    components_without!();
+
+    fn run(&mut self, world: &mut World, resources: &mut Resources) {
+        
+    }
+}
+
+
+fn main() {
+    let mut engine = Engine::new();
+
+    println!("LOL");
+
+    engine.run_cycles(5);
+}
+
+// cargo run --bin snake
